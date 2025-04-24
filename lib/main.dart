924e7c21360eb0249732c636354ba64f2eefccb3
Jaeseok <wnwotjr8@naver.com>
2025-04-24 19:11:29 +0900

    chore: Firebase 초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:alter/core/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Initialize Kakao SDK
   KakaoSdk.init(nativeAppKey: Env.kakaoAppKey);
