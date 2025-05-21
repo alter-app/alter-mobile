15eff1c9539892eaf92f9cd9f5133375102341de
Jaeseok <wnwotjr8@naver.com>
2025-05-21 15:44:11 +0900

    feat: 토근 관리 기능

--- lib/core/secure_storage_provider.dart
+++ lib/core/secure_storage_provider.dart
@@ -1,6 +1,39 @@
+import 'package:alter/feature/auth/model/login_response_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-final secureStorageProvider = Provider<FlutterSecureStorage>(
-  (ref) => const FlutterSecureStorage(),
-);
+final secureStorageProvider = Provider<SecureStorage>((ref) {
+  return SecureStorage();
+});
+
+class SecureStorage {
+  final _storage = const FlutterSecureStorage();
+
+  SecureStorage();
+
+  static const _accessTokenKey = 'access_token';
+  static const _refreshTokenKey = 'refresh_token';
+
+  /// 토큰 저장
+  Future<void> saveToken(String accessToken, String refreshToken) async {
+    await _storage.write(key: _accessTokenKey, value: accessToken);
+    await _storage.write(key: _refreshTokenKey, value: refreshToken);
+  }
+
+  /// 토큰 불러오기
+  Future<ServiceToken?> loadToken() async {
+    final accessToken = await _storage.read(key: _accessTokenKey);
+    final refreshToken = await _storage.read(key: _refreshTokenKey);
+
+    if (accessToken != null && refreshToken != null) {
+      return ServiceToken(accessToken: accessToken, refreshToken: refreshToken);
+    }
+    return null;
+  }
+
+  /// 토큰 삭제
+  Future<void> deleteToken() async {
+    await _storage.delete(key: _accessTokenKey);
+    await _storage.delete(key: _refreshTokenKey);
+  }
+}
